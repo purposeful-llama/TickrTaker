d7d51de4ad59e4ae8f6c52f50ca43426113174c1
lentan <lentan1029@gmail.com>
2016-09-04 21:41:39 -0700

    npm start runs webpack and opens index.html

--- app/start.js
+++ app/start.js
@@ -2,7 +2,28 @@ process.env.NODE_ENV = 'development';
 
 // var path = require('path');
 // var chalk = require('chalk');
-// var webpack = require('webpack');
+var config = require('./webpack.config.js');
+var webpack = require('webpack');
 // var HtmlWebpackPlugin = require('html-webpack-plugin');
-// var WebpackDevServer = require('webpack-dev-server');
+var WebpackDevServer = require('webpack-dev-server');
 // var config = require('./config/webpack.config.js');
+var opn = require('opn');
+
+/********** HELPER FUNCTIONS ************/
+//none
+
+/********** INIT ************/
+var compiler = webpack(config);
+
+var server = new WebpackDevServer(compiler, {
+  contentBase: 'http://localhost/',
+  filename: 'bundle.js',
+  stats: {
+    colors: true
+  },
+  publicPath: '/compiled/',
+});
+
+server.listen(8080, 'localhost', function() {
+  opn('./index.html');
+});
