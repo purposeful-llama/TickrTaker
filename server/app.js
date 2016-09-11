39c915bea4029eec045732c15fc60a05c6d92326
lentan <lentan1029@gmail.com>
2016-09-11 12:30:53 -0700

    reroute 80 to 3000 instead

--- server/app.js
+++ server/app.js
@@ -117,13 +117,13 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-if (process.env.NODE_ENV === 'production') {
-  app.listen(80, function() {
-    console.log('listening on port 80');
-  });
-} else {
-  app.listen(80, function() {
-    console.log('listening on port 3000');
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.listen(80, function() {
+//     console.log('listening on port 80');
+//   });
+// } else {
+app.listen(3000, function() {
+  console.log('listening on port 3000');
+});
+// }
 
