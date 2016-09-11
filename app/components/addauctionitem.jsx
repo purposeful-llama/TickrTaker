3447f529bb2f519a5f162b738d66c730118bed44
k-rathi <kunalrathi1994@gmail.com>
2016-09-11 14:45:38 -0700

    modified the post auction page styling as well as cleaned up a bit of dashboard

--- app/components/addauctionitem.jsx
+++ app/components/addauctionitem.jsx
@@ -23,7 +23,7 @@ export default class AddAuctionItem extends Component {
     });
     
     $('#upload_widget_opener').cloudinary_upload_widget(
-        { cloud_name: 'dijpyi6ze', upload_preset: 'emhtsrjh', theme: 'minimal', folder: 'item_photos' },
+        { cloud_name: 'dijpyi6ze', upload_preset: 'emhtsrjh', theme: 'minimal', folder: 'item_photos', button_class: 'btn btn-primary' },
         function(error, result) { 
           console.log(error, result); 
           context.setState({item: {
@@ -125,98 +125,104 @@ export default class AddAuctionItem extends Component {
     browserHistory.push('/dashboard');
   }
 
-  render() {
+  render() { 
     return (
-    <div id="add-auction-item">
+    <div className="auction-item-container flex-items-xs-center">
+      <div className="col-xs-3"></div>
+      <div id="add-auction-item" className="col-xs-6">
         <form id='item-form' className="form-horizontal" onSubmit={this.submitForm}>
-                  <fieldset>               
-                      <h2>What do you want to sell?</h2>
-                      <div className="control-group">
-                          <label className="control-label">Title</label>
-                          <div className="controls col-xs-12">
-                              <input id="item-title" name="title" type="text" placeholder="Enter a descriptive title" className="input-xlarge" />
-                              <p className="help-block"></p>
-                          </div>
-                          <div className="alert alert-danger fade in" role="alert" id="item-name-error">
-                              <button type="button" className="close">×</button>
-                              <strong>Woah! </strong>Please name the item. <small>Tip: Try to be as descriptive as possible!</small>
-                          </div>
-                      </div>
-                      <div className="control-group">
-                          <label className="control-label">Provide an image</label>
-                          <div className="controls">
-                              <button id="upload_widget_opener" className="btn btn-primary">Upload multiple images</button>
-                              <p className="help-block"></p>
-                          </div>
-                          <div className="alert alert-danger fade in" role="alert" id="item-img-null-error">
-                              <button type="button" className="close">×</button>
-                              <strong>Woah! </strong>Please provide an image. <small>Tip: Upload an image to imgur.com and link it!</small>
-                          </div>
-                          <div className="alert alert-danger fade in" role="alert" id="item-img-valid-error">
-                              <button type="button" className="close">×</button>
-                              <strong>Woah! </strong>Please provide a valid image url. <small>Tip: Copy paste your url again!</small>
-                          </div>
-                      </div>
-                      <div className="control-group">
-                          <label className="control-label">Description</label>
-                          <div className="controls">
-                              <textarea id="item-desc" name="description" type="description" placeholder="Enter a description for the item" />
-                              <p className="help-block"></p>
-                          </div>
-                          <div className="alert alert-danger fade in" role="alert" id="item-desc-error">
-                              <button type="button" className="close">×</button>
-                              <strong>Woah! </strong>Please provide a description. <small>Tip: Items with informative descriptions tend to sell faster!</small>
-                          </div>
-                      </div>
-                      <div className="control-group">
-                          <label className="control-label">End Date</label>
-                          <div className="controls">
-                              <input id="end-date" name="date" type="text" placeholder='end date of auction...' onFocus = {this._onFocus} className="input-xlarge" />
-                              <p className="help-block"></p>
-                          </div>
-                          <div className="alert alert-danger fade in" role="alert" id="item-edate-null-error">
-                              <button type="button" className="close">×</button>
-                              <strong>Woah! </strong>Please provide a valid date. <small>Tip: Give your buyers a good time margin before the item validity expires!</small>
-                          </div>
-                      </div>
-                      <div className="control-group">
-                          <label className="control-label">Current Value</label>
-                          <div id="add-auction-prices" className="controls">
-                              $&nbsp;&nbsp;&nbsp;<input id="current-value" name="curr-val" type="number" step = "0.01" placeholder="Current Value"
-                              className="input-xlarge" />
-                              <p className="help-block"></p>
-                          </div>
-                          <div className="alert alert-danger fade in" role="alert" id="item-cval-null-error">
-                              <button type="button" className="close">×</button>
-                              <strong>Woah! </strong>Please provide a current value price. <small>Tip: Set a reasonable start price.</small>
-                          </div>
-                          <div className="alert alert-danger fade in" role="alert" id="item-cval-over-error">
-                              <button type="button" className="close">×</button>
-                              <strong>Woah! </strong>Current value price must be higher than the end value price. <small>Tip: Set a start price above the end price!</small>
-                          </div>
-                      </div>
+          <fieldset>
+            <h2>What do you want to sell?</h2>
+            <div className="control-group">
+              <label className="control-label">Title</label>
+              <div className="controls">
+                <input id="item-title" name="title" type="text" placeholder="Enter a descriptive title..." className="input-xlarge" />
+                <p className="help-block"></p>
+              </div>
+              <div className="alert alert-danger fade in" role="alert" id="item-name-error">
+                <button type="button" className="close">×</button>
+                <strong>Woah! </strong>Please name the item. <small>Tip: Try to be as descriptive as possible!</small>
+              </div>
+            </div>
+            <div className="control-group">
+              <label className="control-label no-margin">Provide an image</label>
+              <div className="controls">
+                <button id="upload_widget_opener" className="btn btn-primary">Upload multiple images</button>
+                <p className="help-block"></p>
+              </div>
+              <div className="alert alert-danger fade in" role="alert" id="item-img-null-error">
+                <button type="button" className="close">×</button>
+                <strong>Woah! </strong>Please provide an image. <small>Tip: Upload an image to imgur.com and link it!</small>
+              </div>
+            </div>
+            <div className="control-group">
+              <label className="control-label">Description</label>
+              <div className="controls">
+                <textarea id="item-desc" rows='5' name="description" type="description" placeholder="Enter a description for the item..." />
+                <p className="help-block"></p>
+              </div>
+              <div className="alert alert-danger fade in" role="alert" id="item-desc-error">
+                <button type="button" className="close">×</button>
+                <strong>Woah! </strong>Please provide a description. <small>Tip: Items with informative descriptions tend to sell faster!</small>
+              </div>
+            </div>
+            <div className="control-group">
+              <label className="control-label">End Date</label>
+              <div className="controls">
+                <input id="end-date" name="date" type="text" placeholder='end date of auction...' onFocus={ this._onFocus} className="input-xlarge" />
+                <p className="help-block"></p>
+              </div>
+              <div className="alert alert-danger fade in" role="alert" id="item-edate-null-error">
+                <button type="button" className="close">×</button>
+                <strong>Woah! </strong>Please provide a valid date. <small>Tip: Give your buyers a good time margin before the item validity expires!</small>
+              </div>
+            </div>
+            <div className="control-group">
+              <label className="control-label">Current Value</label>
+              <div id="add-auction-prices" className="controls">
+                $&nbsp;
+                <input id="current-value" name="curr-val" type="number" step="0.01" placeholder="Current Value" className="input-xlarge" />
+                <p className="help-block"></p>
+              </div>
+              <div className="alert alert-danger fade in" role="alert" id="item-cval-null-error">
+                <button type="button" className="close">×</button>
+                <strong>Woah! </strong>Please provide a current value price. <small>Tip: Set a reasonable start price.</small>
+              </div>
+              <div className="alert alert-danger fade in" role="alert" id="item-cval-over-error">
+                <button type="button" className="close">×</button>
+                <strong>Woah! </strong>Current value price must be higher than the end value price. <small>Tip: Set a start price above the end price!</small>
+              </div>
+            </div>
+            <div className="control-group">
+              <label className="control-label">Sale Price at End of Auction</label>
+              <div id="add-auction-prices" className="controls">
+                $&nbsp;
+                <input id="end-value" name="end-val" type="number" step="0.01" placeholder="End Value" className="input-xlarge" />
+                <p className="help-block"></p>
+              </div>
+              <div className="alert alert-danger fade in" role="alert" id="item-eval-null-error">
+                <button type="button" className="close">×</button>
+                <strong>Woah! </strong>Please provide an item end value price. <small>Tip: Set the end value price to what you are willing to sell the item for if push comes to shove.</small>
+              </div>
+              <div className="alert alert-danger fade in" role="alert" id="item-eval-over-error">
+                <button type="button" className="close">×</button>
+                <strong>Woah! </strong>End value price must be lower than the current value price.<small>Tip: Set an end price below the start price!</small>
+              </div>
+            </div>
+          </fieldset>
+            <div className = "row auction-button-container">
+              <div className = "col-xs-5">
+              <button type='submit' id='post-item-submit' className="btn btn-primary col-xs">Post Item</button>
+              </div>
+              <div className = "col-xs-5">
+              <button onClick={this.cancelForm} type='button' className="btn btn-primary col-xs">Cancel</button>
+              </div>
+            </div>
+        </form>
+      </div>
+      <div className="col-xs-3"></div>
 
-                      <div className="control-group">
-                          <label className="control-label">Sale Price at End of Auction</label>
-                          <div id="add-auction-prices" className="controls">
-                              $&nbsp;&nbsp;&nbsp;<input id="end-value" name="end-val" type="number" step = "0.01" placeholder="End Value"
-                              className="input-xlarge" />
-                              <p className="help-block"></p>
-                          </div>
-                          <div className="alert alert-danger fade in" role="alert" id="item-eval-null-error">
-                              <button type="button" className="close">×</button>
-                              <strong>Woah! </strong>Please provide an item end value price. <small>Tip: Set the end value price to what you are willing to sell the item for if push comes to shove.</small>
-                          </div>
-                          <div className="alert alert-danger fade in" role="alert" id="item-eval-over-error">
-                              <button type="button" className="close">×</button>
-                              <strong>Woah! </strong>End value price must be lower than the current value price.<small>Tip: Set an end price below the start price!</small>
-                          </div>
-                      </div>
-                  </fieldset>
-                  <button type='submit' id='post-item-submit' className="btn btn-primary" >Post Item</button> <button onClick={this.cancelForm} type='button' className="btn btn-primary" >Cancel</button>
-              </form>
     </div>
+); }
 
-    );
-  }
 }
