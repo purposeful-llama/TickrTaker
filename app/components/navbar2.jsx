f644a96348fded9db043083b4c161b22befd1f8c
Alex Pantelides <alex.pantelides@gmail.com>
2016-09-12 11:59:52 -0700

    Make navbar collapsable

--- app/components/navbar2.jsx
+++ app/components/navbar2.jsx
@@ -6,23 +6,31 @@ export default class Navbar2 extends Component {
     return (
       <nav id="navbar2" className="navbar navbar-dark bg-inverse navbar-fixed-top">
         <Link className="navbar-brand" id="brand-name" to="/"> Tickr </Link>
-        <ul className="nav navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/auctions">Auctions </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link " to="/dashboard">Dashboard<span className="sr-only">(current)</span></Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/makeauction">Post Item</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/usersetting">Settings</Link>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/logout">Logout</a>
-          </li>
-        </ul>
+        <button className="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#nav-content">
+        ☰
+        </button>
+        <div className="collapse navbar-toggleable-xs" id="nav-content">
+          <ul className="nav navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/auctions">Auctions </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link " to="/dashboard">Dashboard</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link " to="/history">History </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/makeauction">Post Item</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/usersetting">Settings</Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/logout">Logout</a>
+            </li>
+          </ul>
+        </div>
       </nav>
     );
   }
