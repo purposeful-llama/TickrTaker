0b37e766e9b14aae0d11064c4334f9304c63d2e0
Wendy <tell.wendy@gmail.com>
2016-09-16 20:12:33 -0700

    add jsx in manageFAQ

--- app/components/manageFAQ.jsx
+++ app/components/manageFAQ.jsx
@@ -5,11 +5,42 @@ export default class ManageFAQ extends Component {
     super(props);
   }
 
+  // componentWillMount() {
+  //   //fetch FAQ, picture and title passing itemId
+  //   // $.ajax({
+  //   //   method: 'GET',
+  //   //   url: '/api/item'
+  //   // })
+
+  // }
+
   render() {
     return (
-      <div>
-        
+      <div className="container auction-item-container">
+      <div className="row">
+        <div className="col-md-4">
+          <img className="img-fluid" src={this.state.Item.picture}></img>
+        </div>
+        <div className="auction-item-details col-md-6 off-set-2">
+          <h2>{thisItem.title}</h2>
+          <br />
+          <hr className="col-md-12 auction-item-title-hr"/>
+          <br />
+          <div className="col-md-12 auctionTitle">
+            <h4>FAQ</h4>
+            {this.state.item.map((item) => {
+              return (
+                  <div>
+                    <p><strong>{this.state.item.questions}</strong></p>
+                    <p>{this.state.item.answer}</p>
+                  </div>
+                );
+              })
+            }
+          </div>
+        </div>
       </div>
-      );
+    </div>
+    )
   }
 }
